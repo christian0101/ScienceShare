19265b09ed45875cf892661c24d4ca73553a7b5a
christian0101 <christian139601@gmail.com>
2019-10-07 10:21:25 +0100

    Initial commit

---
+++ resources/views/enclosure.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+  <title>Enclosures</title>
+</head>
+<body>
+  <h1>{{$animal}} Enclosure</h1>
+  <p>Blah Blah Blah Blah {{$animal}}</p>
+</body>
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return "This is the /home page";
+});
+
+Route::get('/blog', function () {
+    return "This is the /blog page";
+});
+
+Route::get('/home/{name?}', function ($name = null) {
+    return "This is $name's /home page";
+});
+
+Route::get('/enclosures/{animal?}', function ($animal = null) {
+    return view('enclosure', ['animal' => $animal]);
+});
+
+Route::redirect('/here', '/home');
